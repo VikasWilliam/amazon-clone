68765f5b266c8e5267d51e42d76747b461fece59
Vikas William <vikasdenobili@outlook.com>
2021-05-19 23:32:16 +0100

    a

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import './Home.css';
+
+export const Home = () => {
+    return (
+        <div className="Home">
+            <div className="home_container">
+                <img
+                    className='Home_image'
+                    src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+
+                    alt=""
+                />
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDKTRlGFP80Zs21hSLMI1gLver4yZg--ng",
+    authDomain: "clone-a9d46.firebaseapp.com",
+    projectId: "clone-a9d46",
+    storageBucket: "clone-a9d46.appspot.com",
+    messagingSenderId: "247360885880",
+    appId: "1:247360885880:web:6e8e016cbfcb34af46498d",
+    measurementId: "G-JYQQ8FP27D"
+  };
